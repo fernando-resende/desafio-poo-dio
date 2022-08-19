ce088a449b040277159f9fca38ea81b920096b5a
Fernando Resende <fernandohrcampos@yahoo.com.br>
2022-08-19 00:37:17 -0300

    :video_game: Level system based on acquired XP added

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Curso curso1 = new Curso();
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
-        curso1.setCargaHoraria(10000);
+        curso1.setCargaHoraria(10);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
